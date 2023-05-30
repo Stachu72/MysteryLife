8867c5c3ae11c5c111506ab64a00e8d1aa2d13b2
Stachu72 <miloszstachowiak2003@gmail.com>
2023-05-30 23:33:33 +0200

    Initial commit
    First commit with the basic TopDown template

---
+++ README.md
@@ -0,0 +1,3 @@
+# MysteryLife
+
+Developed with Unreal Engine 5
---
+++ Source/MysteryLife/MysteryLifeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MysteryLifeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMysteryLifeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMysteryLifeGameMode();
+};
+
+
+
